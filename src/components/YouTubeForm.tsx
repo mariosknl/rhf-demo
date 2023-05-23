3c2881d7ccde19642c2acc3d687ffeb0f5f6f013
Marios Kanellopoulos <mariosknl@outlook.com>
2023-05-23 13:57:49 +0300

    managing form state & dev tools visualization implemented

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,18 +1,24 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
 const YouTubeForm = () => {
+	const { register, control } = useForm();
+
 	return (
 		<div>
 			<form>
 				<label htmlFor="username">Username</label>
-				<input type="text" id="username" name="username" />
+				<input type="text" id="username" {...register("username")} />
 
 				<label htmlFor="email">Email</label>
-				<input type="email" id="email" name="email" />
+				<input type="email" id="email" {...register("email")} />
 
 				<label htmlFor="channel">Channel</label>
-				<input type="text" id="channel" name="channel" />
+				<input type="text" id="channel" {...register("channel")} />
 
 				<button>Submit</button>
 			</form>
+			<DevTool control={control} />
 		</div>
 	);
 };
