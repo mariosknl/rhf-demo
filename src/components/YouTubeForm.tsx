46ea4697d99b248c979b87ef9e03b5dd41a2759e
Marios Kanellopoulos <mariosknl@outlook.com>
2023-05-23 16:12:32 +0300

    numeric & date values

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -13,6 +13,8 @@ type FormValues = {
 	phNumbers: {
 		number: string;
 	}[];
+	age: number;
+	dob: Date;
 };
 
 const YouTubeForm = () => {
@@ -38,6 +40,8 @@ const YouTubeForm = () => {
 			},
 			phoneNumbers: ["", ""],
 			phNumbers: [{ number: "" }],
+			age: 0,
+			dob: new Date(),
 		},
 	});
 	const { errors } = formState;
@@ -190,6 +194,38 @@ const YouTubeForm = () => {
 							Add phone number
 						</button>
 					</div>
+				</div>
+
+				<div className="form-control">
+					<label htmlFor="age">Age</label>
+					<input
+						type="number"
+						id="age"
+						{...register("age", {
+							valueAsNumber: true,
+							required: {
+								value: true,
+								message: "Age is required",
+							},
+						})}
+					/>
+					<p className="error">{errors.age?.message}</p>
+				</div>
+
+				<div className="form-control">
+					<label htmlFor="dob">Date of Birth</label>
+					<input
+						type="date"
+						id="dob"
+						{...register("dob", {
+							valueAsDate: true,
+							required: {
+								value: true,
+								message: "Date of Birth is required",
+							},
+						})}
+					/>
+					<p className="error">{errors.dob?.message}</p>
 				</div>
 
 				<button>Submit</button>
